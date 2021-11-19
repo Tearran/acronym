0af60ac94d9d5d23dcaa862d309f9384b09b023b
Joseph Turner <Tearran@users.noreply.github.com>
2021-11-19 11:33:00 -0700

    Create buildrootRequierments.sh

---
+++ buildrootRequierments.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#Install requerments
+sudo apt update && sudo apt install -y libncurses5-dev qt5-default glade-gtk2 cvs subversion graphviz git qtcreator sipwitch sed make binutils build-essential gcc g++ bash patch gzip bzip2 perl tar cpio unzip rsync file bc
